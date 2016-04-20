2c19c050ec506d36009d05644a94ac99cab40a82
zhicheng sun <szc1523@163.com>
2016-04-20 05:33:29 -0700

    finish PercolationStats

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -3,21 +3,22 @@
  * for coursera course Algorithms 
  * week 1 Assignment: Percolation Monte Carlo
  */
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.StdOut;
+//import edu.princeton.cs.algs4.StdOut;
 
 public class PercolationStats {
-    private final int len; //length of the grid
-    
+    private final int len, time; //length of the grid, repeated time
+    private final double miu, sigm;
+    private double[] frac; 
     // perform T independent experiments on an N-by-N grid
     public PercolationStats(int N, int T) {
-        len = N;        
-        double[] frac = new double[T]; 
-        int[] count = new int[T];
+        len = N;  
+        time = T;
+        frac = new double[T]; 
         
-        
-        for (int i = 0; i < T; i++) {
+        for (int i = 0; i < time; i++) {
             int cnt = 0;
             Percolation tst = new Percolation(len);
             while (!tst.percolates()) {
@@ -29,33 +30,37 @@ public class PercolationStats {
                 tst.open(row, col);
                 cnt++;
             } 
-            count[i] = cnt;
             frac[i] = (double) (cnt) / len / len;
         }      
-        StdOut.println(StdStats.mean(frac));
-        //StdOut.println(StdStats.stddev(frac));
-        
+        miu = StdStats.mean(frac);
+        sigm = StdStats.stddev(frac);
     }
 
     // sample mean of percolation threshold
-    //public double mean()                      
+    public double mean() {
+        return miu;
+    }
 
     // sample standard deviation of percolation threshold
-    //public double stddev()                    
+    public double stddev() {
+        return sigm;
+    }
     
     // low  endpoint of 95% confidence interval
-   // public double confidenceLo()              
+    public double confidenceLo() {
+        return miu - 1.96 * sigm / Math.sqrt(time);
+    }
 
     // high endpoint of 95% confidence interval
-    //public double confidenceHi()             
-        
-    
-
+    public double confidenceHi() {
+        return miu + 1.96 * sigm / Math.sqrt(time);
+    }
 
     public static void main(String[] args) {
-        //PercolationStats tst = new PercolationStats(200,100); 
-        //for(int i=0; i<8; i++)
-        //  StdOut.println(StdRandom.uniform(8));
+        PercolationStats tst = new PercolationStats(2, 10000);
+        StdOut.println("mean    " + tst.mean());
+        StdOut.println("stddev  " + tst.stddev());
+        StdOut.println("low high" + tst.confidenceLo() + "   " + tst.confidenceHi());
 
     }
 
